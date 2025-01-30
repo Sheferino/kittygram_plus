3e86bef50485cf2f83aab282210d5b0df0e05f79
Dmitry <lmahaon@yandex.ru>
2025-01-30 19:46:02 +0300

    Some feuteres for serializers

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,13 +1,34 @@
+from datetime import datetime
 from rest_framework import serializers
+import webcolors
 
 from .models import Cat, Owner, Achievement, AchievementCat
 
 
+class Hex2NameColor(serializers.Field):
+    # При чтении данных ничего не меняем - просто возвращаем как есть
+    def to_representation(self, value):
+        return value
+
+    # При записи код цвета конвертируется в его название
+    def to_internal_value(self, data):
+        # Доверяй, но проверяй
+        try:
+            # Если имя цвета существует, то конвертируем код в название
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            # Иначе возвращаем ошибку
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        # Возвращаем данные в новом формате
+        return data
+
+
 class AchievementSerializer(serializers.ModelSerializer):
+    ach_name = serializers.CharField(source='name')
 
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'ach_name')
 
 
 class OwnerSerializer(serializers.ModelSerializer):
@@ -22,33 +43,36 @@ class CatSerializer(serializers.ModelSerializer):
     # чтобы показывать владельца по строкому представлению
     # owner = serializers.StringRelatedField(read_only=True)
     achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    # color = Hex2NameColor() # кастомные цвета в RGB
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements',
+                  'age')
 
+    # переопределяем методы create и update, чтобы сохранять achievements
     def create(self, validated_data):
+        # вынимаем achievements
         achievements = validated_data.pop('achievements', None)
-
         cat = Cat.objects.create(**validated_data)
-
+        # сохраняем achievements, если они есть
         if achievements:
             for achievement in achievements:
-                current_achievement, status = Achievement.objects.get_or_create(
+                current_achievement, _ = Achievement.objects.get_or_create(
                     **achievement)
                 AchievementCat.objects.create(
                     achievement=current_achievement, cat=cat)
-
         return cat
 
     def update(self, instance, validated_data):
+        # вынимаем achievements
         achievements = validated_data.pop('achievements', None)
-
+        # сохраняем изменённые поля
         for key, val in validated_data:
             setattr(instance, key, val)
         instance.save()
-
-        # cat = Cat.objects.create(**validated_data)
+        # сохраняем achievements, если они есть
         if achievements:
             for achievement in achievements:
                 current_achievement, _ = Achievement.objects.get_or_create(
@@ -57,3 +81,6 @@ class CatSerializer(serializers.ModelSerializer):
                     achievement=current_achievement, cat=instance)
 
         return instance
+
+    def get_age(self, obj):
+        return datetime.now().year - obj.birth_year
--- cats/views.py
+++ cats/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets, generics
+from rest_framework.decorators import action
 from rest_framework.views import Response, status
+
 
 from .models import Cat, Owner
 from .serializers import CatSerializer, OwnerSerializer
@@ -9,7 +11,19 @@ class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
 
+    # Пишем метод, а в декораторе разрешим работу со списком объектов
+    # и переопределим URL на более презентабельный
+    @action(detail=False, url_path='recent-cats')
+    def recent_white_cats(self, request):
+        # Нужно получить записи о пяти котиках белого цвета
+        cats = Cat.objects.filter(color='Соевый')[:5]
+        # Передадим queryset cats сериализатору
+        # и разрешим работу со списком объектов
+        serializer = self.get_serializer(cats, many=True)
+        return Response(serializer.data)
 
+
+# CBV API для добавления коллекции котов
 class CatAddList(generics.ListCreateAPIView):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
