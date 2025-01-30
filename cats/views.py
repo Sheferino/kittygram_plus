077e3877e1b350ac51b0ff6d9a64c539b2ff1170
Dmitry <lmahaon@yandex.ru>
2025-01-30 16:42:26 +0300

    Add owners, owner API, achievements

--- cats/views.py
+++ cats/views.py
@@ -1,9 +1,26 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
+from rest_framework.views import Response, status
 
-from .models import Cat
-from .serializers import CatSerializer
+from .models import Cat, Owner
+from .serializers import CatSerializer, OwnerSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+
+
+class CatAddList(generics.ListCreateAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, many=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
